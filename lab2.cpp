946a4421c0cab4284047e341e9b7c881887d7553
vaibhavi173 <72758759+vaibhavi173@users.noreply.github.com>
2021-11-30 19:22:55 +0530

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,65 +1,70 @@
 #include<iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
 
-struct Node{
-    public:
-    int key;
-    Node **forward;
-    Node(int key,int level)
-    {
-        this->key = key;
-        forward = new Node*[level+1];
-        
-  
-        memset(forward, 0, sizeof(Node*)*(level+1));
-       
-    }
+struct Node
+{
+  int key;
+  Node **forward;
+  Node(int key,int lvl)
+  {
+    this->key = key;
+    forward = new Node*[lvl+1];
+    memset(forward,0,sizeof(Node*)*(lvl+1));
+
+  }
 };
 
-class SkipList
+class skiplist
 {
-    
-    int max;
-    float P;
-    int level;
-  
-    
-    Node *header;
-public:
-    SkipList(int max, float P)
+  int maxlvl;
+  float p;
+  int level;
+  Node *header;
+
+  public:
+    skiplist(int, float);
+    int randomLevel();
+    Node* createNode(int, int);
+    void insertElement(int);
+    void deleteElement(int);
+    void searchElement(int);
+    void displayList();
+};
+
+skiplist::skiplist(int maxlvl,float p)
+{
+  this->maxlvl = maxlvl;
+  this->p = p;
+  level=0;
+  header = new Node(-1,maxlvl);
+}
+
+int skiplist::randomLevel()
+{
+    float r = (float)rand()/RAND_MAX;
+    int lvl = 0;
+    while(r < p && lvl < maxlvl)
     {
-      this->max = max;
-      this->P = P;
-      level = 0;
-      header = new Node(-1,max);
-        
-    }
-    int randomLevel()
-    {
-     float r = (float)rand()/RAND_MAX;
-     int lvl = 0;
-     while (r < P && lvl < max)
-     {
         lvl++;
         r = (float)rand()/RAND_MAX;
-     }
-     return lvl;
-     }
-    Node* createNode( int key, int level)
-    {
-        Node* n = new Node(key,level);
-        return n;
     }
-    void insertElement(int key)
-    {
-        Node* current = header;
-        Node* update[max+1];
-        memset(update, 0, sizeof(Node*)*(max+1));
-        for(int i=0;i<max+1;i++)
-        cout<<update[i]<<endl;
-        cout<<"new insertion"<<endl;
-        
+    return lvl;
+};
+
+Node* skiplist::createNode(int key, int level)
+{
+    Node *n = new Node(key, level);
+    return n;
+}
+
+void skiplist::insertElement(int key)
+{
+  Node* current = header;
+        Node* update[maxlvl+1];
+        memset(update, 0, sizeof(Node*)*(maxlvl+1));
+      
     for(int i = level; i >= 0; i--)
     {
        
@@ -70,6 +75,8 @@ public:
         cout<<update[i]<<endl;
     }
       current = current->forward[0];
+
+      
       
     if (current == NULL || current->key != key)
     {
@@ -81,8 +88,6 @@ public:
         {
             for (int i=level+1;i<rlevel+1;i++)
                 update[i] = header;
-  
-            
             level = rlevel;
         }
   
@@ -99,38 +104,122 @@ public:
         cout << " succesfully inserted"<<n->key;
         cout<<endl;
      }
-    }
 
-    void displayList()
+}
+
+void skiplist::deleteElement(int key)
+{
+  Node *current = header;
+  Node *update[maxlvl+1];
+  memset(update, 0, sizeof(Node*)*(maxlvl+1));
+  
+  
+  for(int i=level; i>=0; i--)
+  {
+    while(current->forward[i] != NULL  && current->forward[i]->key < key)
+     current = current->forward[i];
+     update[i] = current;
+  }
+  
+  current = current->forward[0];
+  
+  if(current != NULL and current->key == key)
+  {
+    for(int i=0;i<=level;i++)
     {
-       for (int i=0;i<=level;i++)
-     {
+      if(update[i]->forward[i] != current)
+      break;
+      
+      update[i]->forward[i] = current->forward[i];
+      }
+  
+    while(level>0 &&header->forward[level] == 0)
+    level--;
+    cout<<"Successfully deleted key element "<<key<<"\n";
+  }
+  cout<<"Element not in the skiplist "<<key<<"\n";
+};
+  
+
+void skiplist::searchElement(int key)
+{
+    Node *current = header;
+    
+    for(int i = level; i >= 0; i--)
+    {
+        while(current->forward[i] &&
+               current->forward[i]->key < key)
+            current = current->forward[i];
+  
+    }
+    current = current->forward[0];
+  
+    if(current and current->key == key) 
+        cout<<"Found Element: "<<key<<"\n";
+};
+  
+
+void skiplist::displayList()
+{
+    cout<<"\n*****Skip List*****"<<"\n";
+    for(int i=0;i<=level;i++)
+    {
         Node *node = header->forward[i];
-        cout << "Level " << i << ": ";
-        while (node != NULL)
+        cout<<"Level "<<i<<": ";
+        while(node != NULL)
         {
-            cout << node->key<<" ";
+            cout<<node->key<<" ";
             node = node->forward[i];
-
         }
-        cout<<endl;
-        
-     }
-
+        cout<<"\n";
     }
 };
-
+/*
 int main()
 {
     // Seed random number generator
     srand((unsigned)time(0));
   
     // create SkipList object with MAXLVL and P 
-    SkipList lst(2, 0.5);
+    skiplist lst(2, 0.5);
   
     lst.insertElement(3);
     lst.insertElement(6);
     lst.insertElement(7);
-  
+    lst.insertElement(9);
     lst.displayList();
-}  
+}
+*/
+
+//DRIVER CODE
+int main()
+{ 
+  srand((unsigned)time(0));
+  skiplist lst(3, 0.5);
+  int n;
+  int k;
+  cout<<"1:createlist 2:deletelistelement 3:searchlist 4:displaylist"<<endl;
+  cout<<"Enter your choice : ";
+  cin>>n;
+  switch(n)
+  {
+    case 1:cout<<"Enter the key : ";
+           cin>>k;
+           lst.insertElement(k);
+           break;
+    
+    case 2:cout<<"Enter the key : ";
+           cin>>k;
+           lst.deleteElement(k);
+           break;
+    
+    case 3:cout<<"Enter the key : ";
+           cin>>k;
+           lst.searchElement(k);
+           break;
+    
+    case 4: lst.displayList();
+
+  }
+ 
+}
